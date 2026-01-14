a1917a3ceefc30ea0c59c19e94306cd495779445
Nathalia <tatacavalini@gmail.com>
2026-01-13 23:19:33 -0300

    login feito

--- desafio.py
+++ desafio.py
@@ -1,6 +1,6 @@
 #Primeira fase: import
 import pyautogui
-
+import time
 
 pyautogui.PAUSE=1
 
@@ -14,6 +14,13 @@ pyautogui.press("enter")
 #Segundo passo: fazer login
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
+time.sleep(3)
 
 #Terceiro passo: Fazer o login em sí
-
+pyautogui.click(x=491, y=382)
+pyautogui.write("pythonimpressionador@gmail.com")
+pyautogui.press("tab") # passando pro próximo campo
+pyautogui.write("sua senha")
+pyautogui.press("tab")
+pyautogui.press("enter")
+time.sleep(3)
